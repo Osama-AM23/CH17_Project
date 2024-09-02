2f58eea49bb0e0e55db2765bf4bc419210cc74d9
Osama-AM23 <PremkumarM.xworkz@gmail.com>
2024-09-02 14:40:45 +0530

    chaining and override

---
+++ chaining/src/com/xworkz/chaining/Led.java
@@ -0,0 +1,18 @@
+package com.xworkz.chaining;
+
+public class Led extends Light{
+	
+	public void broke()
+	{		
+		System.out.println("Running a Broke in Led ");
+		on();
+	}
+	
+	//over riding 
+	@Override
+	public void on() {
+		System.out.println("Running a on in Led");
+		super.off();
+	}
+	
+}
